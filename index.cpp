f8c1cd1c19f7b7a23cfef9d88266fc4d831e5f8f
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-08-29 09:30:46 -0300

    feature: index

--- index.cpp
+++ index.cpp
@@ -1,1 +1,21 @@
 // teste
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string nome;
+    int idade;
+
+    cout << "Digite seu nome: ";
+    cin >> nome;
+
+    cout << "Digite sua idade: ";
+    cin >> idade;
+
+    cout << "Nome: " << nome << endl;
+    cout << "Idade: " << idade << endl;
+
+    return 0;
+}
