71dc68b12b1faf767568aa163300c2554ef3e526
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-08-29 19:56:17 -0300

    fix: cout da atividade

--- semana02/tabela_ascii.cpp
+++ semana02/tabela_ascii.cpp
@@ -2,16 +2,21 @@
 
 using namespace std;
 
-void tabela(){
-
-    char letra = '0';
-    cout << "A letra " << letra << " corresponde ao valor " << int(letra) << endl;
-
-}
-
 
 int main(void){
-    tabela();
+
+ 
+        cout << "O número " << 0 << " corresponde ao valor " << int('0') << endl;
+        cout << "O número " << 1 << " corresponde ao valor " << int('1') << endl;
+        cout << "O número " << 2 << " corresponde ao valor " << int('2') << endl;
+        cout << "O número " << 3 << " corresponde ao valor " << int('3') << endl;
+        cout << "O número " << 4 << " corresponde ao valor " << int('4') << endl;
+        cout << "O número " << 5 << " corresponde ao valor " << int('5') << endl;
+        cout << "O número " << 6 << " corresponde ao valor " << int('6') << endl;
+        cout << "O número " << 7 << " corresponde ao valor " << int('7') << endl;
+        cout << "O número " << 8 << " corresponde ao valor " << int('8') << endl;
+        cout << "O número " << 9 << " corresponde ao valor " << int('9') << endl;
+
     return 0;
 
 }
