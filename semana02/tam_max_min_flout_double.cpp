4a98eed26e0d00606f6787c49458a29de7e066be
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-08-31 09:33:56 -0300

    feature: retorno maximo e minimo de float, double e casas decimais

---
+++ semana02/tam_max_min_flout_double.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cfloat>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+
+int main(void){
+
+
+    // float pi = 3.14159265358979323846;
+
+    cout << "Tamanho Maximo: " << FLT_MAX << endl;
+    cout << "Tamanho Minimo: " << FLT_MIN << endl;
+    cout << "Tamanho Maximo: " << DBL_MAX << endl;
+    cout << "Tamanho Minimo: " << DBL_MIN<< endl;
+
+
+    float pi = M_PIl;
+    double  pi_double = M_PIl; 
+
+    cout << "Valor Maximo do pi float: " << setprecision(FLT_MAX) << pi << endl;
+    cout << "Valor Maximo do pi double: " << setprecision( DBL_MAX) << pi_double << endl;
+
+    
+    cout << "Valor 2 do pi double: " << setprecision(3) << pi_double << endl;
+    cout << "Valor 4 do pi double: " << setprecision(5) << pi_double << endl;
+    cout << "Valor 8 do pi double: " << setprecision(9) << pi_double << endl;
+    cout << "Valor 16 do pi double: " << setprecision(17) << pi_double << endl;
+
+
+    return 0;
+
+}
