c09e3a7b7ed712280f790bd53ea85c615bfa1d26
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-09-07 20:55:57 -0300

    fix: notas e media

--- semana03/aula09.06.01.cpp
+++ semana03/aula09.06.01.cpp
@@ -1,22 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <iomanip>
+
 
 using namespace std;
 
 int main(){
-    int alunos[3][3];
+    float nota[3][3];
+    int matricula[3];
+    float media[3];
+    float valor_media = 0;
 
     // fazer em casa
     for(int i = 0; i <=2; i++) {
         cout << "Digite a matricula"<<  endl;
-            cin >> alunos[i][0];
+        cin >> matricula[i];
         for(int j = 0; j <=2; j++) {
-            cout << "nota do aluno "<< j+1 <<"°" << endl;
-            cin >> alunos[i][j];
+            cout << "nota do aluno "<< j+1 << endl;
+            cin >> nota[i][j];
 
         }
+       
     }
+
+    for(int i = 0; i <=2; i++) {
+        cout <<"Matricula   " << "   nota01   " << "   nota02  "<<  "  nota03  " <<  "  media  " << endl;
+        cout << "==================================================="  << endl;
+        cout << matricula[i] << "     ";
+        for(int j = 0; j <=2; j++) {
+            cout << "     "<< nota[i][j] << "    ";
+            valor_media = valor_media + nota[i][j] ;
+   
+        }
+        media[i] = valor_media /3;
+        valor_media = 0;
+        cout << setprecision(3) <<media[i] << "    ";
+        cout << endl;
+    }
+
 
     return 0;
 }
