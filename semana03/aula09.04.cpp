c48fbd59132b8f32b462949c3fb8b7e79c1f4ac4
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-09-07 22:33:52 -0300

    fearute: exercicios que faltavam

--- semana03/aula09.04.cpp
+++ semana03/aula09.04.cpp
@@ -1,12 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
 
 using namespace std;
 
-int main(){
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
-    // fazer em casa
+int main() {
+    cout << "Números primos de 1 a 100:" << endl;
+
+    for (int i = 2; i <= 100; i++) {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;
 
     return 0;
 }
